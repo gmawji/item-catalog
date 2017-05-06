87802baecbafc9c23da0807c3fa340e1dd813973
gmawji <git@gmawji.com>
2017-05-06 02:44:36 -0400

    Initial Commit

---
+++ database_init.py
@@ -0,0 +1,107 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import datetime
+from database_setup import *
+
+engine = create_engine('sqlite:///catalog.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+# Delete Categories if exisitng.
+session.query(Category).delete()
+# Delete Items if exisitng.
+session.query(Items).delete()
+
+# Create fake users
+User1 = User(name="Nada Baynom",
+              email="nbaynom0@skype.com",
+              picture='http://dummyimage.com/200x200.png/ff4444/ffffff')
+session.add(User1)
+session.commit()
+
+User2 = User(name="Renado Gress",
+              email="rgress1@t.co",
+              picture='http://dummyimage.com/200x200.png/cc0000/ffffff')
+session.add(User1)
+session.commit()
+
+User3 = User(name="Prinz Blakemore",
+              email="pblakemore2@bluehost.com",
+              picture='http://dummyimage.com/200x200.png/5fa2dd/ffffff')
+session.add(User1)
+session.commit()
+
+User4 = User(name="Ev Beetham",
+              email="ebeetham3@google.com.au",
+              picture='http://dummyimage.com/200x200.png/ff4444/ffffff')
+session.add(User1)
+session.commit()
+
+User5 = User(name="Tamra Eyree",
+              email="teyree4@wix.com",
+              picture='http://dummyimage.com/200x200.png/cc0000/ffffff')
+session.add(User1)
+session.commit()
+
+# Create fake categories
+Category1 = Category(name="Football")
+session.add(Category1)
+session.commit()
+
+Category2 = Category(name="Cars")
+session.add(Category2)
+session.commit
+
+Category3 = Category(name="Snacks")
+session.add(Category3)
+session.commit()
+
+Category4 = Category(name="Gadgets")
+session.add(Category4)
+session.commit()
+
+Category5 = Category(name="Food")
+session.add(Category5)
+session.commit()
+
+# Populate a category with items for testing
+# Using different users for items also
+Item1 = Items(name="Football Boots",
+               date=datetime.datetime.now(),
+               description="Shoes to play football in.",
+               picture="http://bit.ly/2qHbHxd",
+               category=Category1,
+               user_id=1)
+session.add(Item1)
+session.commit()
+
+Item2 = Items(name="Football Shirt",
+               date=datetime.datetime.now(),
+               description="Shirt to play football in.",
+               picture="http://bit.ly/2pb59qn",
+               category=Category1,
+               user_id=2)
+session.add(Item2)
+session.commit()
+
+Item3 = Items(name="Football",
+               date=datetime.datetime.now(),
+               description="A Football.",
+               picture="http://bit.ly/2pJSPR1",
+               category=Category1,
+               user_id=2)
+session.add(Item3)
+session.commit()
+
+print "Your database has been populated with fake data!"
---
+++ database_setup.py
@@ -0,0 +1,62 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    email = Column(String(250), nullable = False)
+    picture = Column(String(250))
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(255), nullable = False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name'          : self.name,
+            'id'            : self.id
+        }
+
+class Items(Base):
+    __tablename__ = 'items'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    date = Column(DateTime, nullable=False)
+    description = Column(String(250))
+    picture = Column(String(250))
+    # Try to use a category with the backref to find all items associated
+    # with that category
+    # (session.query(Category, Items).join(Category.items).all()
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category, backref="items")
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User, backref="items")
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name'          : self.name,
+            'id'            : self.id,
+            'description'   : self.description,
+            'picture'       : self.picture,
+            'category'      : self.category.name
+        }
+
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)
