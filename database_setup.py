1a235abfab1734bb3cc63aabd2ebc7fce56b31ab
gmawji <git@gmawji.com>
2017-05-20 05:00:39 -0400

    User foreginkey and relationship for Category

--- database_setup.py
+++ database_setup.py
@@ -20,6 +20,8 @@ class Category(Base):
 
     id = Column(Integer, primary_key = True)
     name = Column(String(255), nullable = False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
